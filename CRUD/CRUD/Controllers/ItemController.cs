3e5432eb5193c0f3ecaff4df468413a496a0943e
nickbodaveli <nickbodaveli@gmail.com>
2021-11-09 10:20:54 -0800

    Initialize database context

--- CRUD/CRUD/Controllers/ItemController.cs
+++ CRUD/CRUD/Controllers/ItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUD.Data;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,15 @@ namespace CRUD.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
         }
     }
 }
